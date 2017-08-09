4ef192e1042262290c52d2baff993c06f2221be3
Alex Ellsworth <alex.ells21@gmail.com>
2017-08-09 16:29:24 -0600

    working on homework'

--- nesting.js
+++ nesting.js
@@ -33,20 +33,27 @@ var employees = [{
 
 // Code here
 
+function employeeUpdater(){
+   for(var i = employees.length - 1; i >= 0; i--){
+  if(employees[i].firstName === "Theo") {
+    employees.splice(i, 1)
+  }else if(employees[i].firstName === "Lorie"){
+     employees[i].department = "HR"
+
+   }
+   }
+return employees
+}
 
 
 
 
 
-<<<<<<< HEAD:nesting.js
+
+
+
 // The array below represents IDs tied to reported workplace accidents. An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are.
-=======
 
-
-
-
-// The array below represents IDs tied to reported workplace accidents. An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are. 
->>>>>>> master:nestingPractice.js
 // 1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
 // 2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array. Loop over the array from front to back.
 // 3. Return the updated array.
@@ -54,7 +61,6 @@ var employees = [{
 var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 
 // Code here
-
 
 
 
@@ -77,19 +83,10 @@ var cat = {
     }]
 }
 
-<<<<<<< HEAD:nesting.js
-// You need to feed on of Fluffy's friends, Grumpy. Navigate the nested arrays and objects to:
-// 1. invoke the 'feed' method.
-=======
-// Fluffy has two friends, Grumpy and Lazy Bones. 
+
+// Fluffy has two friends, Grumpy and Lazy Bones.
 // 1. Assign the value of Grumpy's 2nd activity to the variable below.
 
 // Code here
 
-var grumpyActivity;
-
-
-
-
-
->>>>>>> master:nestingPractice.js
+var grumpyActivity = cat.catFriends['0'].activities.splice(1, 1)[0];
